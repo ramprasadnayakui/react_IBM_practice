36da3ed897b183c030114e385fd8dbef86947c3c
ramprasadnayakui <ramprasadnayakui@gmail.com>
2022-03-03 00:02:24 +0530

    1 file modified

--- src/components/Rformik.js
+++ src/components/Rformik.js
@@ -1,21 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Formik, Field, Form } from "formik";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Field, Form } from 'formik';
 
-export default function Rformik(){
-  return(
+export default function Rformik() {
+  return (
     <div>
       <h4>React Formik</h4>
-      <blockquote>Formik is a small library that helps you with the 3 most annoying parts in handling forms:<ol><li>Getting values in and out of form state.</li><li>Validation and error messages</li><li>Handling form submission.</li></ol></blockquote>
+      <blockquote>
+        Formik is a small library that helps you with the 3 most annoying parts
+        in handling forms:
+        <ol>
+          <li>Getting values in and out of form state.</li>
+          <li>Validation and error messages</li>
+          <li>Handling form submission.</li>
+        </ol>
+      </blockquote>
       <Formik
-        initialValues={{ name: "", email: "" }}
-        onSubmit={async (values) => {
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
+        initialValues={{ name: '', email: '' }}
+        onSubmit={(values) => alert(values)}
       >
         <Form>
-          <Field name="name" type="text" placeholder="Enter name"/>
+          <Field name="name" type="text" placeholder="Enter name" />
           <Field name="email" type="email" placeholder="Enter mail id" />
           <button type="submit">Submit</button>
         </Form>
@@ -23,5 +28,3 @@ export default function Rformik(){
     </div>
   );
 }
-
-
